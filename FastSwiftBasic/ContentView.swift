e4839ca810c9b1bb6ff898b87429352dd353d6ab
Bullets-jyr <jyr@bullets.co.kr>
2023-10-06 03:33:52 +0900

    01. 변수 - 값을 저장하고 다루는 방법

--- FastSwiftBasic/ContentView.swift
+++ FastSwiftBasic/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    var place: String = "Paris"
+    var name: String = "블렛츠"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(name)님 안녕하세요!")
+            Text("\(name)님의 포인트는 343점 입니다.")
+            Text("\(name)님 안녕히가세요.")
+            Text(name)
         }
         .padding()
     }
