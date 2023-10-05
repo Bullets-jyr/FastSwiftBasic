02ee0a0b036cb95e5a6844a4e9176cab00abb992
Bullets-jyr <jyr@bullets.co.kr>
2023-10-06 03:07:25 +0900

    Initial Commit

---
+++ FastSwiftBasic/FastSwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSwiftBasicApp.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct FastSwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
