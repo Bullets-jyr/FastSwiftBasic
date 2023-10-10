61a2f34770176960dc8a85f8a864afb0c01f8cdc
Bullets-jyr <jyr@bullets.co.kr>
2023-10-10 12:51:56 +0900

    02. 변수의 타입 - 데이터의 종류

---
+++ FastSwiftBasic/VariableType.swift
@@ -0,0 +1,18 @@
+//
+//  VariableType.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/10/23.
+//
+
+import SwiftUI
+
+struct VariableType: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VariableType()
+}
