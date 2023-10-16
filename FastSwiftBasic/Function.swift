8e0305657fa9041f04f2db6bd2d4622e02445eeb
Bullets-jyr <jyr@bullets.co.kr>
2023-10-16 12:27:16 +0900

    06. 함수 - 같은 동작을 하는 방법

---
+++ FastSwiftBasic/Function.swift
@@ -0,0 +1,18 @@
+//
+//  Function.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/16/23.
+//
+
+import SwiftUI
+
+struct Function: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Function()
+}
