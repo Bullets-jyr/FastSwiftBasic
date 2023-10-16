97ae737003e5a6393619852c07a19bb33bae9fa6
Bullets-jyr <jyr@bullets.co.kr>
2023-10-16 12:06:08 +0900

    05. 반복문 - 같은 코드를 반복하는 방법

---
+++ FastSwiftBasic/Loop.swift
@@ -0,0 +1,18 @@
+//
+//  Loop.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/12/23.
+//
+
+import SwiftUI
+
+struct Loop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loop()
+}
