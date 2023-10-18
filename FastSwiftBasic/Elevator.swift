0df911bdc372652a6af12bb450ed0363590262a2
Bullets-jyr <jyr@bullets.co.kr>
2023-10-18 12:27:25 +0900

    01. 구조체 - 하나의 동작을 하는 객체 만들기

---
+++ FastSwiftBasic/Elevator.swift
@@ -0,0 +1,18 @@
+//
+//  Elevator.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/18/23.
+//
+
+import SwiftUI
+
+struct Elevator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Elevator()
+}
