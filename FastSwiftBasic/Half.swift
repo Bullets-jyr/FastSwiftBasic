92fe469449ce46302b57f00ffed62a10fe7c2758
Bullets-jyr <jyr@bullets.co.kr>
2023-10-18 12:02:33 +0900

    07. 중간 점검해보기

---
+++ FastSwiftBasic/Half.swift
@@ -0,0 +1,18 @@
+//
+//  Half.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/16/23.
+//
+
+import SwiftUI
+
+struct Half: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Half()
+}
