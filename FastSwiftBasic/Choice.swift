0d09d106d0addb7c760dd3c202a9e1b37909d07a
Bullets-jyr <jyr@bullets.co.kr>
2023-10-23 12:20:18 +0900

    04. 열거형 - 필요한 선택지를 만드는 방법

---
+++ FastSwiftBasic/Choice.swift
@@ -0,0 +1,18 @@
+//
+//  Choice.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/23/23.
+//
+
+import SwiftUI
+
+struct Choice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Choice()
+}
