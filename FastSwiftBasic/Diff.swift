f58827f517a5c102fda3e1aae28db731d6256f58
Bullets-jyr <jyr@bullets.co.kr>
2023-10-23 11:48:54 +0900

    02. 클래스 - 주소값으로 객체를 만들기

---
+++ FastSwiftBasic/Diff.swift
@@ -0,0 +1,18 @@
+//
+//  Diff.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/18/23.
+//
+
+import SwiftUI
+
+struct Diff: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Diff()
+}
