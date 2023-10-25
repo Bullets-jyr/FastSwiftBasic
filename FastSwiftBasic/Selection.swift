68a854f33a3735e89a8a6ff798cae3edbb932231
Bullets-jyr <jyr@bullets.co.kr>
2023-10-25 12:25:59 +0900

    05. 스위치 - 선택지를 사용해서, 케이스 별로 정리하는 방법

---
+++ FastSwiftBasic/Selection.swift
@@ -0,0 +1,18 @@
+//
+//  Selection.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/24/23.
+//
+
+import SwiftUI
+
+struct Selection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Selection()
+}
