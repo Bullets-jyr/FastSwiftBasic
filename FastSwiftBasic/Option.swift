d953060564480e4657030b8cbbe24c7491e82b4c
xinapse-jyr <jyr@xinapse.ai>
2023-10-30 12:32:54 +0900

    07. 옵셔널 바인딩, 체이닝 - 옵셔널 값을 안전하게 사용하기

---
+++ FastSwiftBasic/Option.swift
@@ -0,0 +1,18 @@
+//
+//  Option.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/30/23.
+//
+
+import SwiftUI
+
+struct Option: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Option()
+}
