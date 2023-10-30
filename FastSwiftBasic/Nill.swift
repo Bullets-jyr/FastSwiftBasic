268bf291019aa94aaa71476a375fb889cfa3a9a9
xinapse-jyr <jyr@xinapse.ai>
2023-10-30 12:02:01 +0900

    06. 옵셔널 - 있을 수 있는 것과 없는 것

---
+++ FastSwiftBasic/Nill.swift
@@ -0,0 +1,18 @@
+//
+//  Nill.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 10/25/23.
+//
+
+import SwiftUI
+
+struct Nill: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Nill()
+}
