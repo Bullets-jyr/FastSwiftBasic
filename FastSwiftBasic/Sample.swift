e588c203c2d6b09a1e50cc8247245c60d3841f65
xinapse-jyr <jyr@xinapse.ai>
2023-11-01 12:20:18 +0900

    08. 실제 만들면서 배운 것들 활용해보기

---
+++ FastSwiftBasic/Sample.swift
@@ -0,0 +1,18 @@
+//
+//  Sample.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 11/1/23.
+//
+
+import SwiftUI
+
+struct Sample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sample()
+}
