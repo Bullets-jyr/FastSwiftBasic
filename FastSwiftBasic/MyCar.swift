eb916f317283a5254c89ff7cdf81d9c05b57e0e7
Bullets-jyr <jyr@bullets.co.kr>
2023-11-06 12:45:52 +0900

    02. 프로토콜 - 설계하고 설계대로 만드는 방법 - (1)

---
+++ FastSwiftBasic/MyCar.swift
@@ -0,0 +1,18 @@
+//
+//  MyCar.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 11/6/23.
+//
+
+import SwiftUI
+
+struct MyCar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCar()
+}
