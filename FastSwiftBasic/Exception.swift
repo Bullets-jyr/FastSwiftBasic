86ebfc76ae98e644ea3099d2ce49c8781a9276ad
Bullets-jyr <jyr@bullets.co.kr>
2023-11-06 12:31:32 +0900

    01. 예외처리 - 만약 에러가 발생한다면

---
+++ FastSwiftBasic/Exception.swift
@@ -0,0 +1,18 @@
+//
+//  Exception.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 11/6/23.
+//
+
+import SwiftUI
+
+struct Exception: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Exception()
+}
