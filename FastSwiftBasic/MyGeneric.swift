fb9910491c4db7266281faf21e296e0b94f4f404
Bullets-jyr <jyr@bullets.co.kr>
2023-11-08 11:58:30 +0900

    03. 제네릭 - 모든 타입을 커버하는 방법

---
+++ FastSwiftBasic/MyGeneric.swift
@@ -0,0 +1,18 @@
+//
+//  MyGeneric.swift
+//  FastSwiftBasic
+//
+//  Created by xinapse on 11/8/23.
+//
+
+import SwiftUI
+
+struct MyGeneric: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyGeneric()
+}
